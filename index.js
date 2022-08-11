e88c21d7486f7b42012f067fb1a428b12063587a
Albert <albert@globalpay.com>
2022-08-11 15:53:34 +0800

    sda

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-console.log(111);
-console.log("test1111f");
+
+console.log("core models test");
+
+import config from "@hrpos/restaurant-core-models";
+
+console.log(config.PorticoURLs.dev);
+
+var a = 3;
+console.log(a);
+
+import { LoyaltyStatus } from "@hrpos/restaurant-core-models";
+
+// import { OnlineGateway } from "@hrpos/restaurant-core-models";
+
+// import MBCustomError from "@hrpos/restaurant-core-models";
---
+++ index2.js
@@ -0,0 +1,8 @@
+
+console.log("core models test");
+
+import config from "@hrpos/restaurant-core-models";
+
+console.log(config.PorticoURLs.dev);
+
+var a = 3;
