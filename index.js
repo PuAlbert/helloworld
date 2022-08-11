8e289dba69684a3f681586debe6871ec2f6f0306
Albert <albert@globalpay.com>
2022-08-11 17:31:27 +0800

    是大龄剩女的嘎

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 
 console.log("core models test");
 
-import config from "@hrpos/restaurant-core-models";
 
-console.log(config.PorticoURLs.dev);
 
 var a = 3;
 console.log(a);
-
-import { LoyaltyStatus } from "@hrpos/restaurant-core-models";
-
-// import { OnlineGateway } from "@hrpos/restaurant-core-models";
-
-// import MBCustomError from "@hrpos/restaurant-core-models";
