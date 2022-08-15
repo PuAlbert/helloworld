1e8c2084288054da94e2b3c34a2e53054d0a244b
Albert <albert@globalpay.com>
2022-08-15 14:19:17 +0800

    sdasd

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,13 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        
+        'type-enum': [
+            2,
+            'always',
+            [
+                'fix', 'feat', 'perf', 'docs', 'chore'
+            ]
+        ],
+        'type-empty': [2, "never"],
     }
 };
