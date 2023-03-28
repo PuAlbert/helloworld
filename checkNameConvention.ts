db9d21797b0a2aa5c0754e70469685ece6cbf2a4
Albert <albert@globalpay.com>
2023-03-28 15:43:39 +0800

    test check name convention

--- checkNameConvention.ts
+++ checkNameConvention.ts
@@ -1,11 +1,15 @@
 import * as core from '@actions/core';
+import { context, GitHub } from '@actions/github'
 
 
 async function run(): Promise<void> {
-    const fakeFile = ["/migrations/a.js", "/migrations/b.js"];
-    core.info(`All:${fakeFile.join(" ")}`);
+    const token = new GitHub(core.getInput('token', {required: true}))
+    console.log(111, core.getInput("token"));
+    console.log(222,token);
+    const files = core.getInput("files", { required: true });
 
-    core.setOutput('all', fakeFile.join(" "));
+    core.info(`All:${files}`);
+    core.setOutput('all', files);
 }
 
 run();
